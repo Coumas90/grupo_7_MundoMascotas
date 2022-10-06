0af78772075e1dae3c8b8bd42e39e35d11f75782
Sebastian Leonel Lio <liosebastianleonel@gmail.com>
2022-10-06 15:45:21 -0300

    estructura inicial - carrito

--- app.js
+++ app.js
@@ -16,3 +16,6 @@ app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '/views/index.html
 
 app.get('/register', (req, res)=> {res.sendFile(__dirname + '/views/register.html');});
 
+app.get('/productCart', (req, res)=> {res.sendFile(__dirname + '/views/productCart.html');});
+
+
