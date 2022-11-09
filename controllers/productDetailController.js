c240b88e6d4ca293c01bc6b00572a3be4ed2e8cb
FacuVega721 <facundovega97@gmail.com>
2022-11-08 22:29:45 -0300

    creación de productos

--- controllers/productDetailController.js
+++ controllers/productDetailController.js
@@ -7,6 +7,35 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const controladorDetalleProducto = {
     detalle: (req, res)=> {res.render('products/productDetail')
     },
-};
+
+    creacion: (req, res) => {res.render('products/creacionProductos');
+	},
+
+    store: (req, res) => {
+		console.log(req.file);
+
+		let productoNuevo = {
+			//id : Math.random() *1000,
+			id : Date.now(),
+			nombre : req.body.nombre,
+            descripción : req.body.descripción,
+            marca : req.body.marca,
+            categoria : req.body.categoria,
+			precio : Number(req.body.precio),
+			descuento : Number(req.body.descuento),
+            colores : req.body.colores,
+            talles : req.body.talles,
+			kilogramos : Number(req.body.kilogramos),
+            seccion : req.body.seccion,
+			mascota : req.body.mascota,
+		}
+
+		products.push(productoNuevo);
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ''));
+
+		res.redirect ('/products');
+}
+}
 
 module.exports = controladorDetalleProducto;
--- routes/productDetail.js
+++ routes/productDetail.js
@@ -5,7 +5,25 @@ const express = require ('express');
 const router = express.Router();
 
 const controladorDetalleProducto = require('../controllers/productDetailController');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination : function(req, file, cb){
+        cb(null, path.join(__dirname, '../../public/images/Productos'))
+        // donde va a ir a imagen fisicamente en /uploads
+        // el destination indica donde va a ir a parar  y el Filename con que nombre
+    },
+    filename : function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        // genero el nombre con el cual se va a guardar el archivo  y datenow para que tenga nombre unico e irrepetible
+    }
+})
+const upload = multer({
+    storage
+})
 
 router.get('/', controladorDetalleProducto.detalle);
+
+router.get('/create', controladorDetalleProducto.creacion); // obtener un producto creado
+router.post('/', upload.single('imagen'), controladorDetalleProducto.store);
 
 module.exports = router;
