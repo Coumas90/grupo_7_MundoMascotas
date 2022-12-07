42217301977ff00eb2d80aba03d23e4dbc337f11
MartinaBoazzo1 <martina.boazzo@solucionet.com.ar>
2022-12-07 19:25:05 -0300

    subiendo

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // Levanto al servidor 3030
-app.listen(3769, () => {console.log('Servidor corriendo')});
+app.listen(37, () => {console.log('Servidor corriendo')});
 
 //Le indicamos a app.js que todas las rutas que inicien con x prefijo tienen que ir a x ruta
 app.use('/', rutasIndex);
