1dde824de4c9a3f5938979d7aeacbeaac3aa1094
MartinaBoazzo1 <martina.boazzo@solucionet.com.ar>
2023-01-30 19:02:41 -0300

    Correccion en conexion a la base de datos, ruta para creacion de tablas y vista de talles

--- database/config/config.js
+++ database/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "Martinamysql1",
     "database": "mundo_mascotas",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "root",
+    "password": "Martinamysql1",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "root",
+    "password": "Martinamysql1",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
