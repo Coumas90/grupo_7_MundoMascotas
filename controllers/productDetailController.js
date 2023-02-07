07a985290c36b12b1bf26726878164c6e99163de
MartinaBoazzo1 <martina.boazzo@solucionet.com.ar>
2023-02-07 15:32:43 -0300

    detalle producto dinamico OK

--- controllers/productDetailController.js
+++ controllers/productDetailController.js
@@ -1,12 +1,12 @@
-
 const db = require("../database/models")
 
 const controladorDetalleProducto = {
     detalle: (req, res)=> {
 		db.Product.findByPk(req.params.id)
-		.then(producto)
+		.then(producto=>{
 		res.render('products/productDetail',{producto})
-    },
+    })
+}
 }
 
 module.exports = controladorDetalleProducto;
